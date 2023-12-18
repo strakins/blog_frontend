4ba0adead7776234a00625aa7310922ae19e2f1f
strakins <akinolablessing25@gmail.com>
2023-12-18 10:59:51 +0100

    Footer page completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <ul className="footer_category">
+        <li> <Link to="posts/categories/:Enginnering">Engineering</Link> </li>
+        <li> <Link to="posts/categories/:Agriculture">Agriculture</Link> </li>
+        <li> <Link to="posts/categories/:Travels">Travels</Link> </li>
+        <li> <Link to="posts/categories/:Art">Art</Link> </li>
+        <li> <Link to="posts/categories/:Weather">Weather</Link> </li>
+      </ul>
+      <div className="footer_copyright">
+        <small>All Rights Reserved &copy; Copyright, Strakins.</small>
+      </div>
+    </footer>
   )
 }
 
